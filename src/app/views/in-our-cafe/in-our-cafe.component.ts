329d241bf46e51b4e815b6cabc0a46eb9862f467
Lourdes Navarro <lourdesnt06@gmail.com>
2022-05-29 14:59:27 +0200

    in our cafe page

--- src/app/views/in-our-cafe/in-our-cafe.component.ts
+++ src/app/views/in-our-cafe/in-our-cafe.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-in-our-cafe',
   templateUrl: './in-our-cafe.component.html',
-  styleUrls: ['./in-our-cafe.component.scss']
+  styleUrls: ['./in-our-cafe.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class InOurCafeComponent implements OnInit {
 
-  constructor() { }
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+
+  fav1: any;
+  fav2: any;
+  fav3: any;
+
+  favourites: boolean[];
+
+  constructor() {
+    var favs = localStorage.getItem('events_favourites');
+    if(favs) {
+      this.favourites = JSON.parse(favs);
+    } else {
+      this.favourites = [false, false, false, false, false];
+      localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    }
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  setFavourite1(fav: boolean){
+    this.favourites[0] = fav;
+    localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    console.log(this.favourites[0]);
+  }
+
+  setFavourite2(fav: boolean){
+    this.favourites[1] = fav;
+    localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    console.log(this.favourites[1]);
+  }
+
+  setFavourite3(fav: boolean){
+    this.favourites[2] = fav;
+    localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    console.log(this.favourites[2]);
+  }
+
+  setFavourite4(fav: boolean){
+    this.favourites[3] = fav;
+    localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    console.log(this.favourites[3]);
+  }
+
+  setFavourite5(fav: boolean){
+    this.favourites[4] = fav;
+    localStorage.setItem('events_favourites', JSON.stringify(this.favourites));
+    console.log(this.favourites[4]);
   }
 
 }
